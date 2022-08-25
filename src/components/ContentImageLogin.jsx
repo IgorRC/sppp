95e4a7597f4b9cf105bfe4c83fc6215ca2d4e6d0
Dana Franchesca Saldaña Espíritu <dasalespi2210@gmail.com>
2022-08-25 17:34:05 -0500

    Cambié el texto

--- src/components/ContentImageLogin.jsx
+++ src/components/ContentImageLogin.jsx
@@ -4,7 +4,7 @@ import "../style-components/ContentImageLogin.css";
 function ContentImageLogin({ estateContet }) {
   return (
     <div className={`contenido ${estateContet ? "" : "traslation-element"}`}>
-      <h1>Aqui es para las imagenes</h1>
+      <h1>Almacena imagenes</h1>
     </div>
   );
 }
