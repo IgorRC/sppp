103e43d116f9b1dcecf96f6683be9f5accd62ff6
unknown <juan.omonte@unas.edu.pe>
2022-11-07 11:30:14 -0500

    agrege componente TI

---
+++ src/components/ComponentePaginaPublica/TecnologiaInformacion.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./StyleComponentePaginaPublica/TecnologiaInformacion.css"
+function TecnologiaInformacion(){
+    return (
+
+        <div className="TecnoInfo">Este es mi componente Sebas</div>
+    );
+}
+
+export default TecnologiaInformacion;
